afd14aafe5fd86ebf1010f9d3b4220298fc36a78
Victoria Ternovaya <victoria.ternovaya@gmail.com>
2025-04-20 14:49:16 +0300

    style: change root div margin

--- fun-chat/src/components/app/app.ts
+++ fun-chat/src/components/app/app.ts
@@ -18,6 +18,7 @@ export default class App {
     private createMainElement(): HTMLElement {
         const mainContainer: HTMLElement = document.createElement('div');
         mainContainer.id = 'root';
+        mainContainer.className = 'root';
         return mainContainer;
     }
 
